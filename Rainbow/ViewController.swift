c5a13d72a297645b0f8f07ad988509d674e23049
Frank Faustino <nawzyah@gmail.com>
2018-06-11 23:35:24 -0700

    Refactor

--- Rainbow/ViewController.swift
+++ Rainbow/ViewController.swift
@@ -2,65 +2,17 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
+    var backgroundColor: UIColor = .white
+
+    func changeColor(with color: UIColor) {
+        backgroundColor == color
+            ? (backgroundColor = .white)
+            : (backgroundColor = color)
     }
 
-    var isWhite = true
-
-    @IBAction func ToggleRed(_ sender: UIButton) {
-        if isWhite == true {
-            isWhite = false
-            view.backgroundColor = .red
-        } else {
-            isWhite = true
-            view.backgroundColor = .white
-        }
-    }
-    @IBAction func ToggleMagenta(_ sender: UIButton) {
-        if isWhite == true {
-            isWhite = false
-            view.backgroundColor = .magenta
-        } else {
-            isWhite = true
-            view.backgroundColor = .white
-        }
-    }
-    @IBAction func ToggleBlue(_ sender: UIButton) {
-        if isWhite == true {
-            isWhite = false
-            view.backgroundColor = .blue
-        } else {
-            isWhite = true
-            view.backgroundColor = .white
-        }
-    }
-    @IBAction func ToggleCyan(_ sender: UIButton) {
-        if isWhite == true {
-            isWhite = false
-            view.backgroundColor = .cyan
-        } else {
-            isWhite = true
-            view.backgroundColor = .white
-        }
-    }
-    @IBAction func ToggleGreen(_ sender: UIButton) {
-        if isWhite == true {
-            isWhite = false
-            view.backgroundColor = .green
-        } else {
-            isWhite = true
-            view.backgroundColor = .white
-        }
-    }
-    @IBAction func ToggleYellow(_ sender: UIButton) {
-        if isWhite == true {
-            isWhite = false
-            view.backgroundColor = .yellow
-        } else {
-            isWhite = true
-            view.backgroundColor = .white
-        }
+    @IBAction func toggleColor(_ sender: UIButton) {
+        changeColor(with: sender.backgroundColor!)
+        view.backgroundColor = backgroundColor
     }
 }
 
